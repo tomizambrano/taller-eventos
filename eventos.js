b6e77648e050903902f8f36e02a01c129c003083
tomizambrano <pzambrano65mvd@gmail.com>
2023-08-09 23:22:52 -0300

    Resolución Dos Alerts

--- eventos.js
+++ eventos.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", () =>{
 
     let divButton = document.querySelector("div");
+    let button = document.querySelector("button");
 
         divButton.addEventListener("click", () =>{
-        alert("Hola!! Soy el div")
-        })
-})
+        alert("Hola!! Soy el div");
+        });
+        button.addEventListener("click", () =>{
+        event.stopPropagation();
+        alert("Holaaa, soy el botón!");
+    });
+});
